9c9dfaccb3bacfcf24042529433a9401acdc7148
Eduardo Michetti <94409995+EduardoMichetti@users.noreply.github.com>
2024-07-07 19:53:31 -0300

    Tela Home: Recuperando valor da variável do session storage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  nomeUsuario: string | null;
 
+  ngOnInit(){
+    this.nomeUsuario = sessionStorage.getItem('usuario');
+  }
 }
